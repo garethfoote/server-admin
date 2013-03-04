b070e3c231663a1d8dccdeeda3d44f60beaa3ea2
vagrant <vagrant@lamp-vm.(none)>
2013-03-04 13:01:28 +0000

    Added install and rm vhost files

---
+++ apache/install.sh
@@ -0,0 +1,6 @@
+sudo ln -s $(pwd)/template.http /etc/apache2/sites-available/template.http
+sudo ln -s $(pwd)/mkvhost /usr/local/bin/mkvhost
+sudo chmod +x /usr/local/bin/mkvhost
+
+sudo ln -s $(pwd)/rmvhost /usr/local/bin/rmvhost
+sudo chmod +x /usr/local/bin/rmvhost
